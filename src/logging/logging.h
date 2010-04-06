9d24459532335a651537ef81e2832cf8895ca0c0
Julian Kranz <julian@juliankranz.de>
2010-04-06 16:59:43 +0200

    Logging: License
    - Added license information

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -7,6 +7,22 @@
  * @brief This file defines all exported data structures, functions, constants and variables pertaining to
  * the logging subsystem.
  */
+/*
+ *  This file is part of Logging.
+ *  
+ *  Logging is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Logging is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Logging.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef LOGGING_H_
 #define LOGGING_H_
